58744dc3c7426c3edfdfaa4437388395f3e39b6d
Dron <knezewski@yandex.ru>
2019-07-16 16:16:44 +0300

    commit

---
+++ src/admin/App.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+  .root-wrapper-container
+    .root-container
+       header.header-container 
+       
+       section.tabs-container 
+       main.content-container
+    .tooltips-container   
+</template>
+
+
+
+<style lang="pcss">
+@import "normalize.css";
+@import "../styles/mixins.pcss";
+@import "../styles/layout/base.pcss";
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800");
+
+
+.root-wrapper-container {
+  height: 100%;
+}
+
+.header-container {
+  background: linear-gradient(to right, #3e3e59, #454573);
+  padding: 15px 0;
+
+  @include phones {
+    padding: 20px 0;
+  }
+}
+
+.root-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.admin-wrapper {
+  display: flex;
+
+  .maincontent {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+}
+
+.content-container {
+  background: url("~images/bg/admin.jpg") center center no-repeat / cover;
+  flex: 1;
+  padding-top: 60px;
+
+  @include phones {
+    padding-top: 30px;
+  }
+}
+
+.page-title {
+  margin-bottom: 60px;
+  font-size: 21px;
+  font-weight: bold;
+}
+
+.tooltips-container {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%, 110%);
+  visibility: hidden;
+  transition: 0.3s;
+
+  &.showed {
+    transform: translate(-50%, 0%);
+    visibility: visible;
+  }
+}
+
+button {
+  background: transparent;
+}
+
+</style>
